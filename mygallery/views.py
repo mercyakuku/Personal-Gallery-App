a4891e4cd066dd23c4fe8e9644fbc4e4adbe8ae1
mercyakuku <mercyakuku24@gmail.com>
2022-05-30 13:17:20 +0300

    Defined the index view function and return render request

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    images = Image.get_all_images()
+    locations = Location.objects.all()
+    title = 'Kush Gallery App'
+
+    return render(request, 'index.html', {'title':title, 'images':images, 'locations':locations})
