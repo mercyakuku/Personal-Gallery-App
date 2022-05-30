0beb70115438b82844af2fdbf651aac40da43eb0
mercyakuku <mercyakuku24@gmail.com>
2022-05-30 16:39:38 +0300

    Defined the application's configurations inside the project and bootstrap in installed apps.

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mygallery.apps.MyGallery.Config',
+    'bootstap5',
 ]
 
 MIDDLEWARE = [
