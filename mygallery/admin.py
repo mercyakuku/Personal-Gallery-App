e95f69462f18a0d53115a15455a870781e484436
mercyakuku <mercyakuku24@gmail.com>
2022-06-01 17:59:01 +0300

    Registered Location and Image models

--- mygallery/admin.py
+++ mygallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Location, Category, Image
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
+
